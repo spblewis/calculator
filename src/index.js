7fe38510fbf29b78cbe700a1a167eb444d0142d3
spblewis <spblewis@gmail.com>
2021-05-23 12:27:06 -0500

    added objects to handle number values

--- src/index.js
+++ src/index.js
@@ -2,8 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const digits = [
+    {
+        "name": "zero",
+        "value": 0
+    },
+    {
+        "name": "one",
+        "value": 1
+    },
+    {
+        "name": "two",
+        "value": 2
+    },
+    {
+        "name": "three",
+        "value": 3
+    },
+    {
+        "name": "four",
+        "value": 4
+    },
+    {
+        "name": "five",
+        "value": 5
+    },
+    {
+        "name": "six",
+        "value": 6
+    },
+    {
+        "name": "seven",
+        "value": 7
+    },
+    {
+        "name": "eight",
+        "value": 8
+    },
+    {
+        "name": "nine",
+        "value": 9
+    }
+]
+
 class Calculator extends React.Component {
 
+    render(){
+
+    return (
+        <h1>initial</h1>
+    );
+
+    };
 }
 
 
