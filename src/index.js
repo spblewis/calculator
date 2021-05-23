ee08d23aae5409d29c9b3ab37688c9d69a79f76b
spblewis <spblewis@gmail.com>
2021-05-23 11:42:33 -0500

    Initial Component/rendering

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Calculator extends React.Component {
+
+}
+
+
+
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('root')
+)
